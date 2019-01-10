8127d5bfc37d9144e24892c02df79c34102df55f
Sunil Kumar <sunilkumar3020@gmail.com>
2019-01-10 14:11:50 +0530

    deleting ripple file

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,17 @@
 $(document).ready(function(){
-    "use strict";
-    $('.slider').ripples({
-        dropRadius: 20,
-        perturbance: 0.01,
-  
-    });
-    
     var typed = new Typed('#typed', {
         stringsElement: '#typed-strings',
         loop: true,
         typeSpeed: 130,
         showCursor: true,
-        cursorChar: '/',
-        autoInsertCss: true,
+        cursorChar: '|',
+        autoInsertCss: true
 
-    });
-    
-    $('.work-section').magnificPopup({
+    }); 
+});
+
+$(document).ready(function(){
+     $('.work-section').magnificPopup({
           delegate: 'a', // child items selector, by clicking on it popup will open
           type: 'image',
           gallery:{enabled:true}
