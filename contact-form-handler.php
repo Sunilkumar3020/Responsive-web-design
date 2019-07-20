b8cf05bfe407f569d9359cc92e45317ecdadd2bc
Sunil kumar <sunilkumar3020@gmail.com>
2019-07-20 20:00:56 +0530

    File Modify

---
+++ contact-form-handler.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$mailFrom = $_POST['email'];
+$message = $_POST['message'];
+
+
+$mailTo = "sunil@regulargk.com";
+$headers = "From: ".$mailFrom;
+$text = "You have received an e-mail from " .$name.". \n\n" .$message;
+
+mail($mailTo, $subject, $text, $headers);
+
+header("Location: index.html?mailsend"); 
+
+
+
+?>
